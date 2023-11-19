9b7cf772c4a17a2f4e4d6599b6a4f13148b919a9
You5if <yousif.ahmed@markoncs.com>
2023-11-19 16:06:19 +0200

    speed errors 3 fixes commit

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -16,30 +16,30 @@ const Footer = () => {
                 <div className={styles.footer_lists}>
                     <div className={styles.footer_pages}>
                         <div className={styles.footer_pages_title}>Pages</div>
-                        <ul className={styles.footer_pages_items}>
-                        <Link href={'/'}><li>Home</li></Link>
-                        <Link href={'/catalog'}><li>Catalog</li></Link>
-                        <Link href={'/about-us'}><li>About</li></Link>
-                        </ul>
+                        <div className={styles.footer_pages_items}>
+                        <Link href={'/'}><ul>Home</ul></Link>
+                        <Link href={'/catalog'}><ul>Catalog</ul></Link>
+                        <Link href={'/about-us'}><ul>About</ul></Link>
+                        </div>
                     </div>
                     <div className={styles.footer_policies}>
                     <div className={styles.footer_pages_title2}>Policies</div>
-                        <ul className={styles.footer_pages_items}>
-                            <Link href={'/exchange-refund-policy'}><li>Exchange & Refund</li></Link>
+                        <div className={styles.footer_pages_items}>
+                            <Link href={'/exchange-refund-policy'}><ul>Exchange & Refund</ul></Link>
                             
-                        </ul>
+                        </div>
                     </div>
                 </div>
             </div>
             <div className={styles.footer_right}>
                 <div className={styles.footer_social}>
                     <div className={styles.footer_pages_title3}>Follow-Us on Social Media</div>
-                        <ul className={styles.footer_social_items}>
-                            <li><AiOutlineInstagram/></li>
-                            <li><AiFillFacebook/></li>
-                            <li><FaXTwitter/></li>
-                            <li><BiLogoTiktok/></li>
-                        </ul>
+                        <div className={styles.footer_social_items}>
+                            <ul><AiOutlineInstagram/></ul>
+                            <ul><AiFillFacebook/></ul>
+                            <ul><FaXTwitter/></ul>
+                            <ul><BiLogoTiktok/></ul>
+                        </div>
                         <div className={styles.footer_pages_title4}><HiOutlineMail className={styles.footer_email}/> Email: <span>two4line@gmail.com</span></div>
                 </div>  
             </div>
